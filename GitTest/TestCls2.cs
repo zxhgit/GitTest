ffd46bfea5a74f0d6e8d578a72fc300888e9f0b5
zxh <agrainofsand2011@126.com>
2016-10-20 11:12:16 +0800

    ccc

--- GitTest/TestCls2.cs
+++ GitTest/TestCls2.cs
@@ -42,7 +42,11 @@ namespace GitTest
             Console.WriteLine("this is branch master");
         }
 
-	    public void C3()
+	public void C3()
+        {
+        }
+        
+        public void C4()
         {
         }
         
